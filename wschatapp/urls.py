645785eefcc2608ccb04be73731c7767c58a5ae7
William Wsilva <williamreis.wsilva@gmail.com>
2023-11-23 17:03:51 -0300

    configurando rotas, e arquivos estaticos

--- wschatapp/urls.py
+++ wschatapp/urls.py
@@ -1,7 +1,17 @@
 from django.urls import path
-from wschatapp.views import Login, Cadastro
+from wschatapp.views import \
+    Login, Cadastro, PerfilPessoal, \
+    PerfilUsuario, Config, Rede, \
+    PostSalvo, MeuPost, NovoPost
 
 urlpatterns = [
-    path("login/", Login),
-     path("cadastro/", Cadastro)
+    path("login/", Login, name='login'),
+    path("cadastro/", Cadastro, name='cadastro'),
+    path("perfil-pessoal/", PerfilPessoal, name='perfil-pessoal'),
+    path("perfil-usuario/", PerfilUsuario, name='perfil-usuario'),
+    path("config/", Config, name='config'),
+    path("rede/", Rede, name='rede'),
+    path("post-salvo/", PostSalvo, name='post-salvo'),
+    path("meu-post/", MeuPost, name='meu-post'),
+    path("novo-post/", NovoPost, name='novo-post'),
 ]
--- wschatapp/views.py
+++ wschatapp/views.py
@@ -5,3 +5,24 @@ def Login(request):
 
 def Cadastro(request):
     return render(request, 'wschatapp/cadastro.html')
+
+def PerfilPessoal(request):
+    return render(request, 'wschatapp/perfil-pessoal.html')
+
+def PerfilUsuario(request):
+    return render(request, 'wschatapp/perfil-usuario.html')
+
+def Config(request):
+    return render(request, 'wschatapp/config.html')
+
+def Rede(request):
+    return render(request, 'wschatapp/rede.html')
+
+def PostSalvo(request):
+    return render(request, 'wschatapp/post-salvo.html')
+
+def MeuPost(request):
+    return render(request, 'wschatapp/meu-post.html')
+
+def NovoPost(request):
+    return render(request, 'wschatapp/novo-post.html')
