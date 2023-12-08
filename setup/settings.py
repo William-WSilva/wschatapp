58643e3c8c97af56fe6a247c8cc8bbe3a5ea6ebd
William Wsilva <williamreis.wsilva@gmail.com>
2023-12-08 12:35:53 -0300

    Configurando aruivo settings: Debug=false, e ordenando middleware.

--- setup/settings.py
+++ setup/settings.py
@@ -36,7 +36,6 @@ MIDDLEWARE = [
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
-    "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
