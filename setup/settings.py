7f01b74e3b319c783e1f47dd1490a92fe8048c80
William Wsilva <williamreis.wsilva@gmail.com>
2023-12-08 15:44:48 -0300

    teste CSRF_TRUSTED_ORIGINS 3

--- setup/settings.py
+++ setup/settings.py
@@ -9,13 +9,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!d6=9(sp36_x)e&72^9&g=p7i5!wtr2y(+cg-i=8(az+t4z!0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'projeto-wschatapp-8e4668852be4.herokuapp.com']
-CSRF_TRUSTED_ORIGINS = ['projeto-wschatapp-8e4668852be4.herokuapp.com',]
+CSRF_TRUSTED_ORIGINS = ['https://projeto-wschatapp-8e4668852be4.herokuapp.com',]
 
 # Application definition
 
