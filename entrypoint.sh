44599913e0923a53ce10ee298ae8640cd24f9ddc
William Wsilva <williamreis.wsilva@gmail.com>
2023-12-08 16:11:18 -0300

    Aumentando tempo de espera no servidor

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
 # Inicia o servidor Django
+echo "Iniciando o servidor Django"
 python manage.py runserver 0.0.0.0:8000 &
 
 # Aguarda o servidor estar pronto
-sleep 5
+echo "Aguardando o servidor estar pronto..."
+sleep 30
 
 # Executa as migrações
+echo "Executando as migrações"
 python manage.py makemigrations
 python manage.py migrate
 
 # Mantém o contêiner em execução
+echo "Contêiner em execução"
 tail -f /dev/null
