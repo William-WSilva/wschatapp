4e4fe8a74f4c67beed24f125706aec9665d61ffa
William Wsilva <williamreis.wsilva@gmail.com>
2023-12-08 15:50:20 -0300

    teste CSRF_TRUSTED_ORIGINS 4

--- setup/settings.py
+++ setup/settings.py
@@ -9,6 +9,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-yskg!_(rz_!k^c%n5fxl!tgde8j!6wn5t4=078ss&-xe676z"
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
