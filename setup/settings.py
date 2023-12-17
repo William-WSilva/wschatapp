36be670f0c311826299e835a1467f8008638bb86
William Wsilva <williamreis.wsilva@gmail.com>
2023-12-17 12:08:59 -0300

    ajustando variaveis ambiente arquivo settings

--- setup/settings.py
+++ setup/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-yskg!_(rz_!k^c%n5fxl!tgde8j!6wn5t4=078ss&-xe676z"
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
